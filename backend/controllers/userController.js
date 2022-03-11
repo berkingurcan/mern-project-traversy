d264d2fb4570427678ef27268aa93f2c2f9771db
berkingurcan <70534820+berkingurcan@users.noreply.github.com>
2022-03-11 20:17:36 +0300

    jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -34,7 +34,8 @@ const registerUser = asyncHandler(async (req, res) => {
         res.status(201).json({
             _id: user.id,
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -45,7 +46,21 @@ const registerUser = asyncHandler(async (req, res) => {
 //Auth User
 //api/users/login
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({message: 'Login User'})
+    const {email, password} = req.body
+
+    const user = await User.findOne({email})
+
+    if(user && (await bcrypt.compare(password, user.password))){
+        res.json({
+            _id: user.id,
+            name: user.name,
+            email: user.email,
+            token: generateToken(user._id)
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid credentials')
+    }
 })
 
 //Get Userdata
@@ -53,6 +68,13 @@ const loginUser = asyncHandler(async (req, res) => {
 const getUser = asyncHandler(async (req, res) => {
     res.json({message: 'Get User'})
 })
+
+//Generate JWT
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: '30d',
+    })
+}
 
 module.exports = {
     registerUser,
